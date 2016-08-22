209ef8107aa1a78153ca1b2db2a803084b2778a5
zhangtiantian10 <975571814@qq.com>
2016-08-22 18:58:13 +0800

    Add express session

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const session = require('express-session');
 
 const app = new express();
 
@@ -8,6 +9,11 @@ const hello = require('./server/routers/hello-world');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('./public'));
+app.use(session({
+  secret: 'a',
+  resave: true,
+  saveUninitialized: true,
+}));
 
 app.use('/', hello);
 
