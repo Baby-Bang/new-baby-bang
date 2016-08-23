7c506e4eab937ca21003dc429398a6ab2e3074fb
zhangtiantian10 <975571814@qq.com>
2016-08-23 14:33:36 +0800

    Write login view is complememt

--- public/js/components/login.js
+++ public/js/components/login.js
@@ -4,19 +4,24 @@ export default class login extends React.Component {
   submitUserInfo() {
     const userName = this.refs.userName.value;
     const password = this.refs.password.value;
-    this.props.onSubmit({userName, password});
-    if(this.props.httpCode === 400) {
-      console.log(this.props.httpCode,2);
+    if(userName && password) {
+      this.props.onSubmit({userName, password});
+      if (this.props.httpCode === 400) {
+        document.getElementById('error').innerHTML = '用户或密码错误！';
+      } else {
+        console.log(this.props.httpCode, 1);
+      }
     } else {
-      console.log(this.props.httpCode, 1);
+      document.getElementById('error').innerHTML = '用户或密码不能为空！';
     }
   }
 
   render() {
     return <div>
-      <input type="text" ref="userName"/>
-      <input type="text" ref="password"/>
-      <button onClick={this.submitUserInfo.bind(this)}>sumbit</button>
+      <input type="text" className="text form-control loginInput center" ref="userName" placeholder="用户名"/>
+      <input type="text" className="text form-control loginInput center" ref="password" placeholder="密码"/>
+      <div className="col-md-6 col-md-offset-1 errorColor" id="error"></div>
+      <button type="button" className="btn btn-default logbtn" onClick={this.submitUserInfo.bind(this)}>sumbit</button>
     </div>
   }
 }
