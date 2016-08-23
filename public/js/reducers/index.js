43020f815b31264744031499ad787be6dbd28bf7
lyf <2531349593@qq.com>
2016-08-23 17:44:33 +0800

    add sign reducers's part

--- public/js/reducers/index.js
+++ public/js/reducers/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import login from './login';
 import reducer from './reducer';
+import sign from './sign'
 
 export default combineReducers({
   login,
-  reducer
+  reducer,
+  sign
 });
