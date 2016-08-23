4cba93f9f3b6c637b97d597e671ae6beee29198f
dingqian <302965061@qq.com>
2016-08-23 15:25:54 +0800

    modified api-diares-test

--- test/apis/show-diaries-test.js
+++ test/apis/show-diaries-test.js
@@ -1,42 +1,34 @@
 const request = require('supertest');
-const clear = require('../../server/helpers/clear');
-const insert = require('../../server/helpers/insert');
 
-describe('show-diaries', () => {
-    let server;
+describe('show diaries', () => {
+  let server;
 
-    beforeEach(function () {
-        insert('userInfo', {diaries:[{
-            id:"",
-            date : "2016-02-10",
-            babyDays : "0-0-3",
-            title : "宝宝今天笑了",
-            content : "在今天中午，给宝宝喂饭，它竟然朝我们笑了",
-            babyScore : 5,
-            parentScore : 4,
-            public : true,
-            likeNumber : 13
-        }]});
-        server = require('../../server');
-    });
+  beforeEach(function () {
+    server = require('../../server');
+  });
 
-    afterEach(function () {
-        clear('userInfo');
-    });
-
-    it('responds to diaries', function(done) {
-        request(server)
-            .get('/showDiaries')
-            .expect(200,[{
-                id:"",
-                date : "2016-02-10",
-                babyDays : "0-0-3",
-                title : "宝宝今天笑了",
-                content : "在今天中午，给宝宝喂饭，它竟然朝我们笑了",
-                babyScore : 5,
-                parentScore : 4,
-                public : true,
-                likeNumber : 13
-            }], done);
-    });
-});
+  it('responds to /diaries', function (done) {
+    request(server)
+      .get('/diaries')
+      .expect(200, [{
+        date: "2016-02-10",
+        babyDays: "0-0-3",
+        title: "宝宝今天笑了",
+        content: "在今天中午，给宝宝喂饭，它竟然朝我们笑了",
+        babyScore: 5,
+        parentScore: 4,
+        public: true,
+        likeNumber: 13
+      }, {
+        date: "2016-02-15",
+        babyDays: "0-0-8",
+        title: "给宝宝买了新床",
+        content: "在今天中午，我们给宝宝买了一张新床",
+        babyScore: 5,
+        parentScore: 4,
+        public: true,
+        likeNumber: 45
+      }], done);
+  });
+})
+;
