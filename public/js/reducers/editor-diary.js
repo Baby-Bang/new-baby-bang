f86bc667c3f5d1a993d14fe94bffc267082771e0
zhangtiantian10 <975571814@qq.com>
2016-08-23 22:44:33 +0800

    Write get baby birthday reducer come ture

--- public/js/reducers/editor-diary.js
+++ public/js/reducers/editor-diary.js
@@ -1,8 +1,14 @@
-module.exports = (state = {httpCode: ''}, action)=> {
+module.exports = (state = {httpCode: '', babyBir: ''}, action)=> {
   switch (action.type) {
     case 'ADD_DIARY':
       return {
-        httpCode: action.httpCode
+        httpCode: action.httpCode,
+        babyBir: state.babyBir
+      };
+    case 'GET_BABYBIR':
+      return {
+        httpCode: state.httpCode,
+        babyBir: action.babyBir
       };
     default:
       return state;
--- public/js/reducers/index.js
+++ public/js/reducers/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import login from './login';
 import reducer from './reducer';
+import editorDiary from './editor-diary';
 
 export default combineReducers({
   login,
-  reducer
+  reducer,
+  editorDiary
 });
