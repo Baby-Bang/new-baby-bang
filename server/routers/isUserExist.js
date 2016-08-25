94c40caba8a9fcaffc480b0114df9e11aca6574d
zhangtiantian10 <975571814@qq.com>
2016-08-25 12:16:48 +0800

    modify all error in eslint

--- server/routers/isUserExist.js
+++ server/routers/isUserExist.js
@@ -4,16 +4,16 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/isUserExist',(req,res)=>{
-    findUser(req.body,(result)=>{
-        if(result.length!=0){
-            req.session.name = result[0].userName;
-            req.session.userInfo = result[0].userInfo;
-            res.json(true);
-        }else{
-            res.json(false);
-        }
-    })
+router.get('/isUserExist', (req, res)=> {
+  findUser(req.body, (result)=> {
+    if (result.length != 0) {
+      req.session.name = result[0].userName;
+      req.session.userInfo = result[0].userInfo;
+      res.json(true);
+    } else {
+      res.json(false);
+    }
+  });
 });
 
-module.exports=router;
+module.exports = router;
--- server/routers/sign.js
+++ server/routers/sign.js
@@ -4,14 +4,14 @@ const express = require('express');
 
 const router = express.Router();
 
-router.post('/session',(req,res)=>{
-    insertUser(req.body, (err) => {
-      let httpCode = 201;
-      if(err) {
-        httpCode = 400
-      }
-      res.status(httpCode).end();
-    });
+router.post('/session', (req, res)=> {
+  insertUser(req.body, (err) => {
+    let httpCode = 201;
+    if (err) {
+      httpCode = 400;
+    }
+    res.status(httpCode).end();
+  });
 });
 
-module.exports=router;
+module.exports = router;
