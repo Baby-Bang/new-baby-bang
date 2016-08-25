86f8abefccb07bafd842d287a3d37427c638d1bd
zxw <Zxw854953678@163.com>
2016-08-25 15:06:04 +0800

    add delete('/sessions') test

--- public/js/reducers/nav.js
+++ public/js/reducers/nav.js
@@ -3,7 +3,7 @@ module.exports = (state = {userName: '', currentTab: 'home'}, action) => {
     case "SET_USERNAME":
       return Object.assign({}, state, {userName: action.userName});
     case 'CHANGE_TAB':
-      return Object.assign({},state,{currentTab:action.text});
+      return Object.assign({}, state, {currentTab: action.text});
     default:
       return state;
   }
--- test/reducers/nav-test.js
+++ test/reducers/nav-test.js
@@ -6,6 +6,6 @@ const expect = require('chai').expect;
 describe('nav reducer', ()=> {
   it('test nav get user name', ()=> {
     const state = {userName: ''};
-    expect(reducer(state, actions.getUserName('test')).userName).to.equal('test');
+    expect(reducer(state, actions.setUserName('test')).userName).to.equal('test');
   });
 });
